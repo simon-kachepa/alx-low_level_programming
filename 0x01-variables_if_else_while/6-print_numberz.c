3c8f820f08d6d38bfe7ba493ab70daca4ed3965c
Simon Kachepa <kachepasimon@gmail.com>
2023-07-14 06:08:23 -0700

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desription: A program that prints all single digit numbers using putchar
+ * Return 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
