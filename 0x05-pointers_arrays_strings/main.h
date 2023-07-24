aee6c3d5ba5fb0c1e0bc2f0ee8d1835853bd949e
Simon Kachepa <kachepasimon@gmail.com>
2023-07-24 06:10:18 -0700

    Create 3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
