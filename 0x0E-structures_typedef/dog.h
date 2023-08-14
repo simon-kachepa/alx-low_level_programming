5ec4a7be38b5195d4f0ada726bbd74261b59c672
Simon Kachepa <kachepasimon@gmail.com>
2023-08-14 06:40:53 -0700

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - structure of the type dog
+ * @name: Pointer to the element name
+ * @age: Pointer to the element age
+ * @owner: Pointer to the element owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
