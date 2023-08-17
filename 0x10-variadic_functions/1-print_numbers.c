1df201faad9d77d7289c66fb0ab741e87a608153
Simon Kachepa <kachepasimon@gmail.com>
2023-08-17 05:46:25 -0700

    Correct 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		if ((index != (n - 1)) && (separator != NULL))
 		{
-			printf("%s ", separator);
+			printf("%s", separator);
 		}
 
 	}
