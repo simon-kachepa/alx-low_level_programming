78a9c0cc6bcdcedfef8e0fede83998ef068dafc5
Simon Kachepa <kachepasimon@gmail.com>
2023-08-29 00:51:21 -0700

    Correct 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	index = 0;
 	ptr = *head;
-	while ((ptr != NULL) && (index < idx))
+	while ((ptr != NULL) && (index < idx - 1))
 	{
 		ptr = ptr->next;
 		index++;
