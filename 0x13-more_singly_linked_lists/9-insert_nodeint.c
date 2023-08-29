b8d7336257f717a7c4857a2ab576281c1048c5c3
Simon Kachepa <kachepasimon@gmail.com>
2023-08-28 23:58:05 -0700

    Betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,5 +35,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp->next = ptr->next;
 	ptr->next = tmp;
 
-	return (tmp? tmp : NULL);
+	return (tmp ? tmp : NULL);
 }
