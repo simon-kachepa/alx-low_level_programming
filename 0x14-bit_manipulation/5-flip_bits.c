4db423f6c6292d134ee7e0257e5529cb6fd643cb
Simon Kachepa <kachepasimon@gmail.com>
2023-08-31 06:34:54 -0700

    Create 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * flip_bits - A function that returns the number of bits you would
+ * need to flip to get from one number to another.
+ * @n: Decimal number
+ * @m: Decimal number
+ * Return: The number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int count, i;
+	unsigned long int number;
+	unsigned long int exclusive;
+
+	exclusive = n ^ m;
+	count = 0;
+	i = 63;
+	while (i >= 0)
+	{
+		number = exclusive >> i;
+		if (number & 1)
+		{
+			count++;
+		}
+		i++;
+	}
+	return (count);
+}
