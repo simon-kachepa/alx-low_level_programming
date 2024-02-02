8519c93ccc0da473567fe12c41e4108a668bc8d9
simon-kachepa <kachepasimon@gmail.com>
2024-02-02 06:22:37 -0800

    Create 1-djb2.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - A hash function implementing the djb2 algorithm.
+ *
+ * @str: String to be used to find the hash value
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned int hash_value = 0;
+	int i = 0;
+
+	while (str[i])
+	{
+		hash_value = (hash_value + str[i]) * 33;
+		i++;
+	}
+
+	return (hash_value);
+}
