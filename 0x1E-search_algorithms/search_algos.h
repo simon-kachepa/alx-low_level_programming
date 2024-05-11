b762d2ea4407bc9baa82cc17beb581efc594e94d
Simon Kachepa <kachepasimon@gmail.com>
2024-05-11 21:00:09 +0000

    Correct search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,10 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+
+#endif
